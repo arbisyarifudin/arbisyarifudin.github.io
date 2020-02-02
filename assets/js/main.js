240f41e36580bfebda7694b747642160fc7bc88d
Arbi Syarifudin <33389879+arbisyarifudin@users.noreply.github.com>
2020-02-03 00:23:13 +0700

    buang file tdk perlu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
 // $(document).ready(function () {
-//     // $('#nav li').click(function () {
-//     //     $('li').removeClass("active");
-//     //     $(this).addClass("active");
-//     // });
-//     $('.menu-utama li > a').each(function () {
-//         if (urlRegExp.test($(this).attr('href'))) {
-//             $(this).addClass('active');
-//         }
-//     });
-// });
 
-// $(function () {
-//     $("li").click(function () {
-//         $("li").removeClass("active");
-//         $(this).addClass("active");
-//     });
 // });
