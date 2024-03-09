437cb5b859e169f941919c0d0e3eab2a4d880a1c
Arbi Syarifudin <arbisyarifudin@gmail.com>
2024-03-10 02:45:57 +0700

    add homepage ui

---
+++ layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <header id="header" class="header">
+            <nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
+                <div class="container">
+                    <router-link class="navbar-brand" to="/">Arsyaf.</router-link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu"
+                        aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="menu">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Work</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Service</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Blog</a>
+                            </li>
+                            <li class="nav-item message">
+                                <a class="nav-link" href="#"><i class="bi bi-envelope"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main id="main" class="main">
+            <RouterView />
+        </main>
+
+        <footer id="footer" class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <p>&copy; 2024 Arsyaf.dev</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: 'Home',
+    link: [
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+        }
+    ]
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="homepage" class="homepage container">
+        <section id="hero" class="section hero">
+            <div class="row align-items-center w-100">
+                <div class="col-lg-7 col-md-8 col-sm-9 col-7">
+                    <div class="introduction">
+                        <h1 class="title">I am Arbi Syarifudin.</h1>
+                        <p class="description">A professional web developer based in Indonesia. <br><small>Specializing
+                                in building (and occasionally designing) exceptional, high-quality websites and
+                                applications.</small></p>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-4 col-sm-3 col-12">
+                    <div class="homepage-photo">
+                        <img src="public/images/photo.png" alt="Arsyaf" class="img-fluid d-none">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="footer-cta" class="section footer-cta">
+            <div class="row">
+                <div class="col-lg-7 col-12">
+                    <div class="latests">
+                        <h2 class="title">My Latest Blog</h2>
+                        <p class="post-title"><a href="#">How to create a beautiful website with Tailwind CSS</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-12">
+                    <div class="contact">
+                        <ul class="contact-row list-unstyled">
+                            <li class="youtube"><a href="#"><i class="bi bi-youtube"></i></a></li>
+                            <li class="instagram"><a href="#"><i class="bi bi-instagram"></i></a></li>
+                            <li class="cta"><a href="#"><span class="text">Work with Me</span> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                                        fill="currentColor" class="bi bi-arrow-right ms-md-2" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                    </svg></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
